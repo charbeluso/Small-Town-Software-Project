b6870ebf8dc819073d66e1f4cd904fcfe801466f
charbeluso <charmbeluso@gmail.com>
2020-05-29 09:27:24 -0500

    added final iOS draft

---
+++ CSTC-iOS-Survey/CSTC-iOS-Survey/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CSTC-iOS-Survey
+//
+//  Created by Charmaine Beluso on 5/29/20.
+//  Copyright © 2020 MSU Carl Small Town Center. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
